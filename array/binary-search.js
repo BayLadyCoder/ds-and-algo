4b6e0d3037afda20f98cff9a18807d1ae590bed4
BayLadyCoder <rachada_32@hotmail.com>
2023-10-21 17:59:15 -0400

    Array binary search

---
+++ array/binary-search.js
@@ -0,0 +1,19 @@
+const binarySearch = (nums) => {
+  let left = 0;
+  let right = nums.length - 1;
+  let mid;
+
+  while (left <= right) {
+    mid = Math.floor((left + right) / 2);
+
+    if (nums[mid] === target) {
+      return mid;
+    } else if (nums[mid] > target) {
+      right = mid - 1;
+    } else {
+      left = mid + 1;
+    }
+  }
+
+  return -1;
+};
